28ea1dfa2b02799c86749d0b0d7692d824046c84
Eliot Kent Woodrich <ekwoodrich@gmail.com>
2019-10-24 21:39:25 -0700

    Fixed incompatible dependencies for react-native-firebase.

--- android/app/src/main/java/com/thinkwith/MainApplication.java
+++ android/app/src/main/java/com/thinkwith/MainApplication.java
@@ -3,6 +3,7 @@ package com.thinkwith;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
