1b7be77e0e9971688835c0dcc05d329462539616
Eliot Kent Woodrich <ekwoodrich@gmail.com>
2020-01-10 01:37:38 -0800

    Tweaked margins on NoteViewer.

--- components/home/NoteViewer.js
+++ components/home/NoteViewer.js
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
     borderWidth: 0.5,
     borderColor: 'lightgray',
     marginBottom: 5,
-    marginTop: 5,
+    marginTop: -10,
     marginLeft: 10,
     marginRight: 10
   }
